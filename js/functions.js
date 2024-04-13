35fada1e3a9d07cb3f89a4a0d585a325970aab56
Esdras Ceballos Zurita <esdras.ceballosz@outlook.com>
2024-04-13 11:48:03 -0600

    added form validation

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,60 @@
-export function searchingWeather ( event ){
+import { mainContainer, cityField, countryField, submitButton } from "./selectors.js";
+
+// Searching Weather
+
+export function searchingWeather(event) {
     event.preventDefault();
 
-    console.log('Buscando clima...');
+    //validating form
+
+    if (cityField.value === '' || countryField.value === '') {
+
+        alertMessage('Ambos campos son obligatorios');
+
+        return;
+
+    } else if (Number(cityField.value)) {
+
+        alertMessage('Ingresa un nombre de Ciudad válido');
+
+        return;
+    }
+};
+
+// Show alert message
+
+function alertMessage(message) {
+
+    // check existing alert
+
+    const alertMessage = document.querySelector('.alert-message');
+
+
+    // create alert
+
+    if (!alertMessage){
+
+        const boxAlert = document.createElement('div');
+
+        boxAlert.classList.add('bg-red-100', 'border-red-400', 'text-red-400', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center', 'alert-message');
+
+        boxAlert.innerHTML = `
+            <strong class="font-bold">Error!</strong><span class="block">${message}</span>
+        `;
+
+        submitButton.disabled = true;
+        submitButton.classList.add('opacity-600');
+        mainContainer.appendChild(boxAlert);
+
+        // remove alert
+
+        setTimeout(() => {
+
+            boxAlert.remove();
+            submitButton.classList.remove('opacity-600');
+            submitButton.disabled = false;
+        }, 3000);
+
+    }
+
 };
